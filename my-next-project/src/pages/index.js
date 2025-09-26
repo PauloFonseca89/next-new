56960b225a8a129b3caed17ce95c1176a63bb0e3
Paulo Fonseca <39968977+PauloFonseca89@users.noreply.github.com>
2025-09-26 15:34:28 +0100

    update

--- my-next-project/src/pages/index.js
+++ my-next-project/src/pages/index.js
@@ -1,20 +1,27 @@
 import ComponenteComCondicional from "@/components/ComponenteComCondicional";
 import ComponenteComEventos from "@/components/ComponenteComEventos";
 import ComponenteComMap from "@/components/ComponenteComMap";
+import ComponenteComState from "@/components/ComponenteComState";
 import ComponenteComUmaProp from "@/components/ComponenteComUmaProp";
+import ComponenteComUseEffect from "@/components/ComponenteComUseEffect";
 import ComponenteComVariasProps from "@/components/ComponenteComVariasProps";
 import PrimeiroComponente from "@/components/PrimeiroComponente";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-blue-500 flex flex-col items-center
-justify-center">
-<h1 className="text-3xl font-bold text-black">Resumo do React😀</h1>
-<PrimeiroComponente />
-<ComponenteComUmaProp  nome= "Paulo Fonseca"/>
-<ComponenteComVariasProps tilulo="Perfil do Utilizador" cor="blue" nome="Renato" idade="32"/>
-<ComponenteComCondicional/>
-<ComponenteComMap/>
-<ComponenteComEventos/>
-</div>
-)}
+    <div className="min-h-screen color-white">
+      <div className="max-w-xl mx-auto">
+        <h1 className="text-3xl font-bold text-black">Resumo do React😀</h1>
+        <PrimeiroComponente />
+        <ComponenteComUmaProp nome="Paulo Fonseca" />
+        <ComponenteComVariasProps titulo="Perfil do Utilizador" cor="blue" nome="Renato" idade="32" />
+        <ComponenteComCondicional />
+        <ComponenteComMap />
+        <ComponenteComEventos />
+        <ComponenteComState />
+        <ComponenteComUseEffect />
+        <ComponenteComFormix />
+      </div>
+    </div>
+  );
+}
